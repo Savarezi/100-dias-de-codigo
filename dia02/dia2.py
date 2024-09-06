72fb4015d0e0223db825e8d72809dcb507bd1929
Savarezi <patriciasavarezioliveira@gmail.com>
2024-09-06 18:41:52 -0300

    dia 2

--- dia02/dia2.py
+++ dia02/dia2.py
@@ -1,5 +1,5 @@
-num1 = float(input("Digiti o Primeiro Número : "))
-num2 = float(input("Digiti o Segundo Número : " ))
+num1 = float(input("Digiti o Primeiro número : "))
+num2 = float(input("Digiti o Segundo número : " ))
 
 resultado = num1 + num2 
 
