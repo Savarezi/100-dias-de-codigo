f2be654b23508854e17ab73212c3fa091e8e87d8
Savarezi <patriciasavarezioliveira@gmail.com>
2024-09-09 17:18:18 -0300

    dia 4

---
+++ dia04/dia04.py
@@ -0,0 +1,28 @@
+
+
+## CALCULADORA
+
+def adicao(x,y):
+    return x + y
+
+def subtraçao(x,y):
+    return x - y
+
+
+def multiplicacao (x,y):
+    return x * y 
+
+def divisao (x,y):
+    return x / y 
+
+
+def calculadora ():
+    print ("Selecione a operação.")
+    print ("1 Adição")
+    print ("2 Subtração")
+    print ("3 Multiplicação")
+    print ("4 Divisão")
+
+num1 = int(input("Digite o primeiro número:  "))
+num2 = int(input("Digite o segundo número :  "))
+  
